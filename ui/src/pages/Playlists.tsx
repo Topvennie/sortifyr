e99c27081dc36739bbd5198c089b3225a0cb03b3
Topvennie <vincent@vallaeys.com>
2025-11-28 15:37:44 +0100

    feat(playlist): save view selection in cookie

--- ui/src/pages/Playlists.tsx
+++ ui/src/pages/Playlists.tsx
@@ -3,20 +3,34 @@ import { PlaylistTreeView } from "@/components/playlist/PlaylistTreeView"
 import { Switch, Title } from "@mantine/core"
 import { useState } from "react"
 
+type View = "table" | "tree"
+
+const storageKey = "music-playlist-view"
+
 export const Playlists = () => {
-  const [treeView, setTreeView] = useState(false)
+  const [view, setView] = useState<View>(localStorage.getItem(storageKey) as View ?? "table")
+
+  const handleCheckToggle = () => {
+    let newView: View
+
+    if (view === "table") newView = "tree"
+    else newView = "table"
+
+    localStorage.setItem(storageKey, newView)
+    setView(newView)
+  }
 
   return (
     <div className="flex flex-col gap-8">
       <div className="flex items-center justify-between">
         <Title order={1}>Playlists</Title>
         <Switch
-          checked={treeView}
-          onChange={e => setTreeView(e.target.checked)}
+          checked={view == "tree"}
+          onChange={handleCheckToggle}
           label="Tree view"
         />
       </div>
-      {treeView
+      {view === "tree"
         ? <PlaylistTreeView />
         : <PlaylistTableView />
       }
